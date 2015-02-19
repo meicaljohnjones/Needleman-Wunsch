05e0a817049c29ca4515053985269fb7b7786f07
Mike Jones <mjclackjones@gmail.com>
2015-02-19 11:56:20 +0000

    Fix calculate.value.matrix existing tests to add extra column and extra row to value matrix

--- R/sequence_alignment.R
+++ R/sequence_alignment.R
@@ -1,7 +1,7 @@
 calculate.value.matrix <- function(first.sequence, second.sequence) {
-   value.matrix <- matrix(ncol=length(first.sequence), nrow=length(second.sequence))
-   colnames(value.matrix) <- first.sequence
-   rownames(value.matrix) <- second.sequence
+   value.matrix <- matrix(ncol=length(first.sequence) + 1, nrow=length(second.sequence) + 1)
+   colnames(value.matrix) <- c('*', first.sequence)
+   rownames(value.matrix) <- c('*', second.sequence)
 
    value.matrix
 }
--- tests/testthat/test.sequence_alignment.R
+++ tests/testthat/test.sequence_alignment.R
@@ -1,18 +1,21 @@
-test_that('calculate.value.matrix generates matrix with length(first.sequence) columns and length(second.sequence) rows', {
+test_that('calculate.value.matrix generates matrix with length(first.sequence) + 1 columns and length(second.sequence) + 1 rows', {
     first.sequence <- c('a','a')
     second.sequence <- c('b','b')
 
     output.matrix <- calculate.value.matrix(first.sequence, second.sequence)
 
-    expect_that(ncol(output.matrix), equals(length(first.sequence)))
-    expect_that(nrow(output.matrix), equals(length(second.sequence)))
+    expect_that(ncol(output.matrix), equals(length(first.sequence) + 1))
+    expect_that(nrow(output.matrix), equals(length(second.sequence) + 1))
 })
 
-test_that('calculate.value.matrix labels col names using first.sequence and row names using second sequence', {
+test_that('calculate.value.matrix labels col names using first.sequence and row names using second sequence, however, prepending an asterisk element row and column', {
     first.sequence <- c('a','a')
     second.sequence <- c('b','b')
 
+    expected.cols <- c('*', first.sequence)
+    expected.rows <- c('*', second.sequence)
+
     output.matrix <- calculate.value.matrix(first.sequence, second.sequence)
-    expect_that(first.sequence, equals(colnames(output.matrix)))
-    expect_that(second.sequence, equals(rownames(output.matrix)))
+    expect_that(expected.cols, equals(colnames(output.matrix)))
+    expect_that(expected.rows, equals(rownames(output.matrix)))
 })
